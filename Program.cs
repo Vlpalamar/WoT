5a71d523b67e64c16fcbe7860413659658dbc8e7
Vlpalamar <65705755+Vlpalamar@users.noreply.github.com>
2020-09-28 00:20:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+using System.Threading;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tank_length=5;
+            Tank[] T34=new Tank[tank_length];
+            Tank[] pantera=new Tank[tank_length];
+
+           
+            for (int i = 0; i < tank_length; i++)
+            {
+                Tank t = new Tank("T34");
+                Thread.Sleep(50); //без  этого метода у танков параметры одинаковые и постоянно  ничья происходит
+                Tank p = new Tank("Pantera");
+                T34[i] = t;
+                pantera[i] = p;     
+            }
+
+            for (int i = 0; i < tank_length; i++)
+            {
+                Console.Write($"Battle №{i+1}: ");
+                int result=-2;
+                result = T34[i] * pantera[i];
+                if (result == -1)
+                    Console.WriteLine($"танк {T34[i].Name} победил танк {pantera[i].Name}");
+                else if (result == 0)
+                    Console.WriteLine("ничья");
+                else
+                    Console.WriteLine($"танк {pantera[i].Name} победил танк {T34[i].Name}");
+
+            }
+             
+            
+            Console.Read();
+         
+        }
+    }
+}
---
+++ Tank.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public   class Tank
+    {
+        public string Name { get; set; }
+        protected int Ammo_lvl { get; set; }
+        protected int Armor_lvl { get; set; }
+        protected int Maneuv_lvl { get; set; } 
+
+        public Tank(string name="T34")
+        {  
+            Name = name;
+            var rand = new Random();
+            Ammo_lvl = rand.Next(1, 100);
+            Armor_lvl = rand.Next(1, 100);
+            Maneuv_lvl = rand.Next(1, 100);
+        }
+
+        public static int operator *(Tank t1, Tank t2)
+        {
+            int victory_count1=0;
+            int victory_count2=0;
+
+            if     (t1.Ammo_lvl > t2.Ammo_lvl)
+                victory_count1++;
+            else if(t1.Ammo_lvl<t2.Ammo_lvl)
+                victory_count2++;
+
+            if      (t1.Armor_lvl > t2.Armor_lvl)
+                victory_count1++;
+            else if (t1.Armor_lvl < t2.Armor_lvl)
+                victory_count2++;
+
+            if      (t1.Maneuv_lvl > t2.Maneuv_lvl)
+                victory_count1++;
+            else if (t1.Maneuv_lvl < t2.Maneuv_lvl)
+                victory_count2++;
+
+
+            if      (victory_count1 == victory_count2)
+                return 0;
+            else if (victory_count1 > victory_count2)             
+                return -1;           
+            else
+                return 1;
+            
+           
+
+
+
+        }
+
+
+
+    }
+}
